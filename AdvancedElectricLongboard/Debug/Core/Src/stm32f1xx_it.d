82182bb6038fc61db36ae5662caf5f1b33360564
pundisan <56048032+pundisan@users.noreply.github.com>
2019-10-31 23:17:14 +0100

    Add files via upload

---
+++ AdvancedElectricLongboard/Debug/Core/Src/stm32f1xx_it.d
@@ -0,0 +1,79 @@
+Core/Src/stm32f1xx_it.o: ../Core/Src/stm32f1xx_it.c \
+ C:/Users/doemu/workspace/AdvancedElectricLongboard/Core/Inc/main.h \
+ C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
+ C:/Users/doemu/workspace/AdvancedElectricLongboard/Core/Inc/stm32f1xx_hal_conf.h \
+ C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
+ C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h \
+ C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h \
+ C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h \
+ C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/CMSIS/Include/core_cm3.h \
+ C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/CMSIS/Include/cmsis_version.h \
+ C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h \
+ C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h \
+ C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h \
+ C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h \
+ C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_exti.h \
+ C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h \
+ C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h \
+ C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_can.h \
+ C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h \
+ C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_adc.h \
+ C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_adc_ex.h \
+ C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h \
+ C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
+ C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
+ C:/Users/doemu/workspace/AdvancedElectricLongboard/Core/Inc/stm32f1xx_it.h
+
+C:/Users/doemu/workspace/AdvancedElectricLongboard/Core/Inc/main.h:
+
+C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
+
+C:/Users/doemu/workspace/AdvancedElectricLongboard/Core/Inc/stm32f1xx_hal_conf.h:
+
+C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h:
+
+C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h:
+
+C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h:
+
+C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h:
+
+C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/CMSIS/Include/core_cm3.h:
+
+C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/CMSIS/Include/cmsis_version.h:
+
+C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/CMSIS/Include/cmsis_compiler.h:
+
+C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
+
+C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h:
+
+C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h:
+
+C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h:
+
+C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_exti.h:
+
+C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h:
+
+C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h:
+
+C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_can.h:
+
+C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h:
+
+C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_adc.h:
+
+C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_adc_ex.h:
+
+C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h:
+
+C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h:
+
+C:/Users/doemu/workspace/AdvancedElectricLongboard/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
+
+C:/Users/doemu/workspace/AdvancedElectricLongboard/Core/Inc/stm32f1xx_it.h:
